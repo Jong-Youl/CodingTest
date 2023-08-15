686766be51ed442ab10fcf4b0304d93cb8ca5924
Jongyoul <112914969+Jong-Youl@users.noreply.github.com>
2023-08-15 19:27:43 +0900

    [Gold V] Title: 치킨 배달, Time: 148 ms, Memory: 13708 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,70 +1,83 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+class Node{
+	int r, c;
+
+	public Node(int r, int c) {
+		this.r = r;
+		this.c = c;
+	}
+	
+}
 
 public class Main {
-	static StringTokenizer st;
-	static int N, M;
-	static int ans = Integer.MAX_VALUE;
-	static int[][] chickenGetList;
-	static List<int[]> house;
-	static List<int[]> chicken;
+	
+	static int N;//도시 크기
+	static int M;//치킨집 수
+	static List<Node> chicken = new ArrayList<>();
+	static int[][] live;//살아남은 치킨
+	static List<Node> person = new ArrayList<>();
+	private static int min = Integer.MAX_VALUE;
 	
 	public static void main(String[] args) throws IOException {
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		st= new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		chicken = new LinkedList<>();
-		house = new LinkedList<>();
+		N = Integer.parseInt(st.nextToken());// 지도 크기
+		M = Integer.parseInt(st.nextToken());// 살아 남은 치킨 집 
+
+		int [][] arr = new int[N][N];// 치킨 정보
 		
-		for (int i = 0; i < N; i++) {
+		for (int r = 0; r < N; r++) {
 			st = new StringTokenizer(br.readLine());
-			
-			for (int j = 0; j < N; j++) {
-				int temp = Integer.parseInt(st.nextToken());
+			for (int c = 0; c < N; c++) {
+				arr[r][c] = Integer.parseInt(st.nextToken());
 				
-				if(temp == 2) {
-					int[] temp2 = {i, j};
-					chicken.add(temp2);
-				} else if (temp == 1) {
-					int[] temp1 = {i,j};
-					house.add(temp1);
+				if (arr[r][c] == 2) {
+					chicken.add(new Node(r, c));
 				}
+				else if (arr[r][c] == 1) {
+					person.add(new Node(r, c));
+				}
+				
 			}
-		}
-		chickenGetList = new int[chicken.size()][2];
-		comb(0,0);
-		System.out.println(ans);
-	}
-	
-	public static void comb(int cnt, int start) {
-		if (cnt == M) {
-			diffDistance();
-			return;
-		}
-
-		for (int i = start; i < chicken.size(); i++) {
-			chickenGetList[cnt][0] = chicken.get(i)[0];
-			chickenGetList[cnt][1] = chicken.get(i)[1];
-			comb(cnt + 1, i + 1);
 		}
 		
+		live = new int [M][2];
+		chicken_distance(0, 0);
+		System.out.println(min);
 	}
 	
-	public static void diffDistance() {
-		int minDistance = 0;
-
-		for (int i = 0; i < house.size(); i++) {
-			int min = Integer.MAX_VALUE;
-			for (int j = 0; j < M; j++)	{
-				min = Math.min(min, Math.abs(chickenGetList[j][0] - house.get(i)[0]) + 
-						            Math.abs(chickenGetList[j][1] - house.get(i)[1]));
+	public static void chicken_sum() {
+		int res = 0;
+		
+		for(int i=0; i<person.size(); i++) {//고객마다의
+			int p_min = Integer.MAX_VALUE;//최소거리
+			for(int j=0; j<M; j++) {
+				p_min = Math.min(p_min, Math.abs(person.get(i).r - live[j][0]) + Math.abs(person.get(i).c - live[j][1]));
 			}
-			minDistance += min;
+			res += p_min;
 		}
-		ans = Math.min(ans, minDistance);
+		min = Math.min(min, res);
 		return;
 	}
-}
+
+	public static void chicken_distance(int index, int depth) {
+		//탈출
+		if(depth == M) {//치킨집 조합의 길이가 M개가 됬다면
+			chicken_sum();
+			return;
+		}
+		
+		for(int i=index; i<chicken.size();i++) {//치킨집 조합 뽑기
+				live[depth][0] = chicken.get(i).r;
+				live[depth][1] = chicken.get(i).c;
+				chicken_distance(i+1, depth+1);
+			}
+		}
+	}
