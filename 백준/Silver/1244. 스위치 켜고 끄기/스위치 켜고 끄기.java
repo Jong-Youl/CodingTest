da3dbd4f6189d33a3f3d8c171757bd4878ed7cf6
Jongyoul <112914969+Jong-Youl@users.noreply.github.com>
2023-08-19 19:09:24 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 84 ms, Memory: 11812 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -1,67 +1,70 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-
-		int N = sc.nextInt();// 스위치 개수 //9
-		int[] arr = new int[N];// 스위치 배열 생성
-
-		for (int i = 0; i < N; i++) {// 스위치 배열 값 대입
-			arr[i] = sc.nextInt();
+	public static StringTokenizer st;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		boolean [] swich = new boolean [N+1];
+		
+		//스위치 배열 입력 완료
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			int num = Integer.parseInt(st.nextToken());
+					
+			if(num == 1) {
+				swich[i+1] = !swich[i+1];
+			}				
 		}
+		
+		int rep = Integer.parseInt(br.readLine());//숫자 받는 횟수
 
-		int std = sc.nextInt();// 학생 수
+		for(int i=0; i<rep; i++) {
+			st = new StringTokenizer(br.readLine());
 
-		for (int i = 0; i < std; i++) {
-			int gender = sc.nextInt();// 학생 성별
-			int num = sc.nextInt();// 스위치 번호 // 3
-
-			if (gender == 1) {// 남자일 때
-					for (int j = 1; j <= (N / num); j++) {// 전체 (스위치수/받은수) -> 배수개수만큼 반복
-						if (arr[(j * num) - 1] == 0) {// 0이면 1로 1이면 0으로
-							arr[(j * num) - 1] = 1;
-						} else {
-							arr[(j * num) - 1] = 0;
-						}
-					}
-				} 
-			if (gender == 2) {// 여자일 때
-				//제자리 변환
-				if (arr[num - 1] == 0) {
-					arr[num - 1] = 1;
-				} else {
-					arr[num - 1] = 0;
-				}
-				for (int j = 1; j <= (N / 2); j++) {// 스위치 수 나누기 2해서 그것만큼 양쪽으로 대칭이라고 생각
-					// 인덱스 오류 방지 해줘야함
-					if (num + j - 1 < N  && num - j - 1 >= 0) {// 범위안
-						if (arr[num + j - 1] != arr[num - j - 1]) {// 대칭확인 아니면 끝
-							break;
-						} else if (arr[num + j - 1] == arr[num - j - 1]) {// 대칭이면 바꿔주기
-							if (arr[num + j - 1] == 1) {
-								arr[num + j - 1] = 0;
-							} else {
-								arr[num + j - 1] = 1;
-							}
-							if (arr[num - j - 1] == 1) {
-								arr[num - j - 1] = 0;
-							} else {
-								arr[num - j - 1] = 1;
-							}
-						}
-					}
-					else {
-						break;
-					}
+			int gender = Integer.parseInt(st.nextToken());//성별
+			int num = Integer.parseInt(st.nextToken());//받은 숫자
+			
+			if(gender == 1) {// 남학생이면
+				for(int j = 1; j<=N/num; j++) {//존재하는 배수만큼 반복
+					swich[j*num] = !swich[j*num];
 				}
 			}
-
-		}
-		for(int i = 0; i<N; i++) {
-			System.out.print(arr[i] + " ");
-			if ((i+1) % 20 == 0)
+			
+			if(gender == 2) {// 여학생이면
+				swich[num] = !swich[num];//해당 스위치 바꿈
+				int cnt = 0;
+				
+				for(int j = 1; j<=N/2; j++) {//양옆으로 늘어남	
+					if(num+j<=N && num-j >=1 && swich[num+j] == swich[num-j]) {//조건 만족시
+						cnt++;
+					}
+					else break;//아니면 탈출
+				}
+				
+				for(int j=1;j<=cnt;j++) {
+					swich[num+j] = !swich[num+j]; 
+					swich[num-j] = !swich[num-j];
+				}
+			}
+		}//스위치 만지기 종료
+		
+		for(int i=1; i<=N; i++) {
+			if(swich[i]) {
+				System.out.printf(1 + " ");
+			}
+			else {
+				System.out.printf(0 + " ");
+			}
+			
+			if(i%20 == 0) {
 				System.out.println();
+			}
 		}
 	}
 }
