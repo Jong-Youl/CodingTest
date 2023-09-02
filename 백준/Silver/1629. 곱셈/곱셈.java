52d5da003bb8e7a457079a3cf2da2b661fe1a661
Jongyoul <112914969+Jong-Youl@users.noreply.github.com>
2023-09-02 21:37:17 +0900

    [Silver I] Title: 곱셈, Time: 80 ms, Memory: 11476 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.java
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -1,38 +1,34 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
- 
+
 public class Main {
- 
+	
 	public static long C;
 	
-	public static void main(String[] args) throws IOException {
-		
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		long A = Long.parseLong(st.nextToken());
 		long B = Long.parseLong(st.nextToken());
 		C = Long.parseLong(st.nextToken());
 		
-		System.out.println(pow(A, B));
+		System.out.println(pow(A,B));	
 	}
 	
-	public static long pow(long A, long exponent) {
-		
-		if(exponent == 1) {
-			return A % C;
+	public static long pow(long c, long n) {
+		if(n == 1) {//1일때는 그냥 씀
+			return c % C;
 		}
 		
-		long temp = pow(A, exponent / 2);
-
-		if(exponent % 2 == 1) {
-			return (temp * temp % C) * A % C;
+        long tmp = pow(c, n/2);
+        
+		if(n % 2 == 0) {
+			return tmp * tmp % C;
 		}
-		return temp * temp % C;
+		return (tmp * tmp % C) * c % C;
 		
 	}
-	
 }
