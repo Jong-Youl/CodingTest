f09894ed8290fe0d56063d2f2c77fe2a06edef77
Jongyoul <112914969+Jong-Youl@users.noreply.github.com>
2023-09-26 15:23:23 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1004 ms, Memory: 298952 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -1,23 +1,42 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-        
-		int N = Integer.parseInt(br.readLine());
-		
-		for(int i = 0; i<N;i++) {
-			st = new StringTokenizer(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int T = Integer.parseInt(st.nextToken());
+
+		for (int tc = 1; tc <= T; tc++) {
+
+			String str = br.readLine();
+
+			String[] arr2 = str.split(" ");
+
+			int[] arr3 = new int[arr2.length];
+
+			for (int i = 0; i < arr3.length; i++) {
+
+				arr3[i] = Integer.parseInt(arr2[i]);
+			}
+
+			int k = arr3[0] + arr3[1];
+
+//			System.out.println(k);
+
+			bw.write(k+"\n");
 			
-			int tmp1 = Integer.parseInt(st.nextToken());
-			int tmp2 = Integer.parseInt(st.nextToken());
-			sb.append(tmp1 + tmp2+"\n");
 		}
-		System.out.println(sb);
+		bw.flush();
+		bw.close();
 	}
+
 }
