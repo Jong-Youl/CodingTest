18ace19c27827ff321cfa8fa8962504e5e09c150
Jongyoul <112914969+Jong-Youl@users.noreply.github.com>
2023-11-15 15:47:58 +0900

    [Bronze III] Title: 오븐 시계, Time: 104 ms, Memory: 12840 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		
+		if(B + C >= 60) {
+            A += ((B+C)/60);
+			B = ((B+C)%60);
+		}else {
+			B += C;
+		}
+		
+		if(A >= 24) {
+			A = A%24;
+		}
+		System.out.println(A+" "+B);
+	}
+}
