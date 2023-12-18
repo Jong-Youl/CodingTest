4cc409bd87a5ca8e9274a9eb4e5ea11f0ea36766
Jongyoul <112914969+Jong-Youl@users.noreply.github.com>
2023-12-18 14:46:16 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 512 ms, Memory: 19536 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -1,29 +1,29 @@
 import java.util.Scanner;
 
-
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
 		
-		for(int i = 1; i<=N; i++) {
-			for(int j = 0; j<N-i; j++) {
-				System.out.print(" ");
-			}
-			for(int j = 0; j<i*2-1; j++) {
-				System.out.print("*");
-			}
-			System.out.println();
-		}
-		for(int i = N-1; i >= 0 ; i--) {
-			for(int j = 0; j<N-i; j++) {
-				System.out.print(" ");
-			}
-			for(int j = 0; j<i*2-1; j++) {
-				System.out.print("*");
-			}
-			System.out.println();
-		}
-		
+        int N = sc.nextInt();
+        
+        for(int i=1; i<=N-1; i++) {
+        	for(int j=N-i; j>0; j--) {
+        		System.out.print(" ");
+        	}
+        	for(int j=0; j<2*i-1; j++) {
+        		System.out.print("*");
+        	}
+        	System.out.println();
+        }
+        
+        for(int i=1; i<=N;i++){//빈칸 출력 0부터4까지
+        	for(int j=0; j<i-1; j++){//i=1일때 출력 x
+        		System.out.print(" ");
+        		}
+        	for(int j=2*(N-i)+1; j>0 ;j--){//처음엔 2n-1개 마지막엔 1개
+        		System.out.print("*");
+        		}
+        System.out.println();
+        }
 	}
 }
