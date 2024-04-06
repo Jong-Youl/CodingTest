45f802fdc763ca1e41a7ec95355eecb7ebaa6684
Jongyoul <dlawhdfbf12@hs.ac.kr>
2024-04-06 17:15:36 +0900

    [Silver I] Title: 스타트와 링크, Time: 244 ms, Memory: 35728 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,70 +1,70 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
- 
+
 public class Main {
-	
-	static int N;
-	static int[][] arr;
-	static boolean[] visit;	
-	static int min = Integer.MAX_VALUE;
-	
+	public static int N;
+	public static int min = Integer.MAX_VALUE;
+	public static boolean [] visit;
+	public static int [][] scoreMap;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
+		StringTokenizer st;
+
 		N = Integer.parseInt(br.readLine());
- 
-		arr = new int[N][N];
-		visit = new boolean[N];
- 
- 
-		for (int r = 0; r < N; r++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for (int c = 0; c < N; c++) {
-				arr[r][c] = Integer.parseInt(st.nextToken());
+		visit = new boolean[N + 1];
+		scoreMap = new int [N+1][N+1];
+
+		// 배열 입력
+		for(int r = 1; r <=N; r++){
+			st = new StringTokenizer(br.readLine());
+			for(int c = 1; c <=N; c++){
+				scoreMap[r][c] = Integer.parseInt(st.nextToken());
 			}
 		}
- 
-		start_link(0, 0);
+
+		comb(0, 1);
+
 		System.out.println(min);
- 
 	}
-	static void start_link(int idx, int depth) {
-		if(depth == N / 2) {//depth가 최대로 채워지면
-			score();//점수 반환
-			return;//종료
-		}
- 
-		for(int i = idx; i < N; i++) {
-			if(!visit[i]) {
-				visit[i] = true;	// 방문 체크
-				start_link(i + 1, depth + 1);// 재귀
-                visit[i] = false;
-			}
-		}
-	}
- 
-	static void score() {
-		int start_score = 0;
-		int link_score = 0;
-        int diff = 0;
-		for (int i = 0; i < N - 1; i++) {
-			for (int j = i + 1; j < N; j++) { 
-				if (visit[i] == true && visit[j] == true) {//둘 다 true면 start팀
-					start_score += arr[i][j];
-					start_score += arr[j][i];
+
+	public static void comb(int depth, int idx) {
+		if(depth == N/2) {
+			int [] memberA = new int [N/2];
+			int [] memberB = new int [N/2];
+			int idxA = 0;
+			int idxB = 0;
+			int totalA = 0;
+			int totalB = 0;
+
+			for(int i = 1; i <= N; i++){
+				if(visit[i]) {
+					memberA[idxA++] = i;
 				}
-				else if (visit[i] == false && visit[j] == false){//둘 다 false면 false 팀
-					link_score += arr[i][j];
-					link_score += arr[j][i];
+				else {
+					memberB[idxB++] = i;
 				}
 			}
+
+			for(int i = 0; i < N/2; i++){
+				for(int j = 0; j < N/2; j++){
+					if(i == j) continue;
+					totalA += scoreMap[memberA[i]][memberA[j]];
+					totalB += scoreMap[memberB[i]][memberB[j]];
+				}
+			}
+			min = Math.min(min, Math.abs(totalA - totalB));
+			return;
 		}
-		// 두 팀의 점수 차이
-		diff = Math.abs(start_score - link_score);
-		min = Math.min(diff, min);
-				
+
+		for(int i = idx; i <= N; i++) {
+			if(visit[i]) continue;
+
+			visit[i] = true;
+			comb(depth + 1, i + 1);
+			visit[i] = false;
+		}
 	}
- 
 }
