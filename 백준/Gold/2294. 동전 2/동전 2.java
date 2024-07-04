a6141d6e8bf9ae0caa90c2dbf31205a5547a0ea8
Jongyoul <dlawhdfbf12@naver.com>
2024-07-04 10:41:55 +0900

    [Gold V] Title: 동전 2, Time: 100 ms, Memory: 11788 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -5,39 +5,33 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	public static int [] dp, coin;
-	public static int inf = 987654321;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		
-		coin = new int [N];
-		dp = new int [K+1];
-		Arrays.fill(dp, inf);
-		
-		for(int i = 0; i < N; i++) {
-			coin[i] = Integer.parseInt(br.readLine());
-			if(coin[i] > K) continue;
-			dp[coin[i]] = 1;
-		}
-		
-		Arrays.sort(coin);
-		
-		for(int i = 0; i < N; i++) {
-			if(coin[i] == 0) continue;
-			if(i < N-1 && coin[i] == coin[i+1]) continue;
-			for(int j = coin[i]; j <= K; j++) {
-				dp[j] = Math.min(dp[j], dp[j - coin[i]] + 1);
-			}
-		}
-		
-		int res = dp[K] == inf ? -1 : dp[K];
-		System.out.println(res);
-	}
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int [] dp = new int [K + 1];
+        int [] coin = new int [N];
+
+        dp[0] = 0;
+
+        for (int i = 0; i < N; i++) {
+            coin[i] = Integer.parseInt(br.readLine());
+            if(coin[i] <= K) dp[coin[i]] = 1;
+        }
+
+        // 가치
+        for(int i = 0; i < K; i++) {
+            if(dp[i] == 0) continue;
+            // 동전
+            for(int j = 0; j < N; j++) {
+                if(i + coin[j] <= K && (dp[i + coin[j]] == 0 || dp[i + coin[j]] > dp[i] + 1))
+                    dp[i + coin[j]] = dp[i] + 1;
+            }
+        }
+        System.out.println(dp[K] == 0? -1:dp[K]);
+    }
 }
