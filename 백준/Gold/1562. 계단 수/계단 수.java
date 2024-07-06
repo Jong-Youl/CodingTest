5173d17f836b7b82efb3314257ad69dfeecf778a
Jongyoul <dlawhdfbf12@naver.com>
2024-07-06 16:15:10 +0900

    [Gold I] Title: 계단 수, Time: 140 ms, Memory: 22764 KB -BaekjoonHub

--- 백준/Gold/1562. 계단 수/계단 수.java
+++ 백준/Gold/1562. 계단 수/계단 수.java
@@ -1,39 +1,38 @@
 import java.util.Scanner;
 
 public class Main {
+    private static final int modNum = 1000000000;
 
-	static int MOD = 1_000_000_000; 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		long[][][] dp = new long[n+1][11][1<<10];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-		// 1<<i : 0~9 비트마스킹
-		for(int i=1; i<10; i++) {
-			dp[1][i][1<<i] =1;
-		}
+        int N = sc.nextInt();
 
-		for(int i=2; i<n+1; i++) {
-			for(int j=0; j<10; j++) {
-				for(int k=0; k<1024; k++) {
-					int bit = k | (1 << j);
-					if(j==0) {
-						dp[i][j][bit] = (dp[i][j][bit] + dp[i-1][j+1][k]) % MOD;
-					}
-					else if(j==9) {
-						dp[i][j][bit] = (dp[i][j][bit] + dp[i-1][j-1][k]) % MOD;
-					}
-					else {
-						dp[i][j][bit] = (dp[i][j][bit] + dp[i-1][j+1][k]+ dp[i-1][j-1][k]) % MOD;
-					}
-				}
-			}
-		}
+        long[][][] dp = new long[N + 1][11][1 << 10];
 
-		long sum = 0;
-		for(int i=0; i<10; i++) {
-			sum = (sum + dp[n][i][1023]) % MOD;
-		}
-		System.out.println(sum);
-	}
+        for(int i=1; i<10; i++) {
+            dp[1][i][1<<i] =1;
+        }
+
+        for (int i = 2; i <= N; i++) {
+            for (int j = 0; j <= 9; j++) {
+                for (int k = 0; k < (1 << 10); k++) {
+                    int bit = k | (1 << j);
+                    if (j == 0)
+                        dp[i][j][bit] = (dp[i][j][bit] + dp[i - 1][j + 1][k]) % modNum;
+                    else if (j == 9)
+                        dp[i][j][bit] = (dp[i][j][bit] + dp[i - 1][j - 1][k]) % modNum;
+                    else
+                        dp[i][j][bit] = (dp[i][j][bit] + dp[i - 1][j + 1][k] + dp[i - 1][j - 1][k]) % modNum;
+                }
+            }
+        }
+
+        long sum = 0L;
+        for (int i = 0; i < 10; i++) {
+            sum = (sum + dp[N][i][1023]) % modNum;
+        }
+
+        System.out.println(sum);
+    }
 }
