d414ead365885f0c8aa95bc8dca8b6402d0fb4f6
Jongyoul <dlawhdfbf12@naver.com>
2024-07-10 10:38:30 +0900

    Update 동전 분배.java

--- 백준/Gold/1943. 동전 분배/동전 분배.java
+++ 백준/Gold/1943. 동전 분배/동전 분배.java
@@ -1,46 +1,48 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    private static int totalValue;
+    private static int[] value;
 
     public static void main(String[] args) throws IOException {
-        for (int i = 0; i < 3; i++) {
-            System.out.println(distribute() ? 1 : 0);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        for (int tc = 0; tc < 3; tc++) {
+            int N = Integer.parseInt(br.readLine());
+
+            totalValue = 0;
+            value = new int[100_001];
+
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
+                int coin = Integer.parseInt(st.nextToken());
+                int cnt = Integer.parseInt(st.nextToken());
+
+                checkValue(coin, cnt);
+                for (int j = 1; j <= cnt; j++)
+                    value[coin * j]++;
+                totalValue += (coin * cnt);
+            }
+            if (totalValue % 2 != 0) sb.append(0).append("\n");
+            else sb.append(value[totalValue / 2] != 0 ? 1 : 0).append("\n");
         }
+
+        System.out.println(sb);
     }
 
-    private static boolean distribute() throws IOException {
-        int[] coins = new int[100001];
-
-        int n = Integer.parseInt(br.readLine());
-        int total = 0;
-        for (int i = 1; i <= n; i++) {
-            String[] input = br.readLine().split(" ");
-            int coin = Integer.parseInt(input[0]);
-            int count = Integer.parseInt(input[1]);
-            for (int j = 0; j <= count; j++) {
-                for (int k = total; k >= 0; k--) { 
-                    if (k + (coin * j) > 100000) { 
-                        break;
-                    }
-
-                    if (coins[k] > 0) { 
-                        coins[k + (coin * j)] += 1;
-                    }
-                }
+    private static void checkValue(int coin, int cnt) {
+        for (int i = totalValue; i > 0; i--) {
+            if (value[i] == 0) continue;
+            for (int j = 1; j <= cnt; j++) {
+                if (i + (coin * j) > 100_000) break;
+                value[i + (coin * j)]++;
             }
-            for (int j = 0; j <= count; j++) { 
-                coins[coin * j] += 1;
-            }
-            total += coin * count; 
         }
-
-        if (total % 2 != 0) { 
-            return false;
-        }
-
-        return coins[total / 2] != 0; 
     }
 }
