6abb64638e6242bf35d920e36c36acf79f13a00c
Jongyoul <dlawhdfbf12@naver.com>
2024-07-19 09:48:21 +0900

    [Gold IV] Title: 테트로미노, Time: 840 ms, Memory: 35152 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.java
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -10,18 +10,18 @@ public class Main {
 
     public static void main(String [] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        String str = br.readLine();
-        N = Integer.parseInt(str.split(" ")[0]);
-        M = Integer.parseInt(str.split(" ")[1]);
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
         max = 0;
         map = new int [N][M];
         visit = new boolean[N][M];
 
         for(int r = 0; r < N; r++) {
-            str = br.readLine();
+            st = new StringTokenizer(br.readLine());
             for(int c = 0; c < M; c++) {
-                map[r][c] = Integer.parseInt(str.split(" ")[c]);
+                map[r][c] = Integer.parseInt(st.nextToken());
             }
         }
 
