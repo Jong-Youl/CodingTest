8f63f27a9a1702ea3adbbe701182559e04621cb8
Jongyoul <dlawhdfbf12@naver.com>
2024-09-21 15:26:53 +0900

    Update 좋다.java

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -23,7 +22,7 @@ public class Main {
         // 음수가 섞임
         cnt = 0;
         mergeSort(arr, 0, arr.length);
-        
+
         for (int i = 0; i < N; i++) {
             int target = arr[i];
             int left = 0, right = N - 1;
@@ -51,21 +50,6 @@ public class Main {
         }
 
         System.out.println(cnt);
-    }
-
-    private static void binarySearch(int [] arr, int start, int end, int target) {
-        int mid = (start + end) / 2;
-
-        if(start >= end) return;
-
-        if(arr[mid] == target) {
-            int idx = 0;
-            // mid 주변의 값이 Target이랑 같을수록 cnt++;
-        }
-        else if(arr[mid] < target)
-            binarySearch(arr, start, mid, target);
-        else
-            binarySearch(arr, mid, end, target);
     }
 
     private static void mergeSort(int [] arr, int start, int end) {
