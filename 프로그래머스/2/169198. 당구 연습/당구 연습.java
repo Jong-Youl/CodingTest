f5b12656bfd8ded5b314a6ca3ca098c37da5c429
Jongyoul <dlawhdfbf12@naver.com>
2024-11-23 13:08:21 +0900

    [level 2] Title: 당구 연습, Time: 0.80 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/2/169198. 당구 연습/당구 연습.java
+++ 프로그래머스/2/169198. 당구 연습/당구 연습.java
@@ -1,38 +1,31 @@
 class Solution {
     public int[] solution(int m, int n, int startX, int startY, int[][] balls) {
-        int[] answer = new int[balls.length];
+        int[] answer = new int [balls.length];
         int idx = 0;
         
-        for (int[] target : balls) {
+        for (int [] target : balls) {
+            // 피타고라스 문제임, 벽에 부딪힐 때 사방을 다 비교
+            // 상, 하, 좌, 우
             int min = Integer.MAX_VALUE;
-
-            // 4가지 방향에 대해 각각 검토
-            // 위쪽 벽 반사
-            if (!(startY < target[1] && startX == target[0])) {
-                if (!isBlocked(startX, startY, target[0], target[1], m, n, 0)) {
-                    min = Math.min(min, getDistance(m, n, startX, startY, target[0], target[1], 0));
+            for(int dir = 0; dir < 4; dir++) {
+                if(dir == 0) {
+                    if(startY < target[1] && startX == target[0]) 
+                        continue;
                 }
-            }
-
-            // 아래쪽 벽 반사
-            if (!(startY > target[1] && startX == target[0])) {
-                if (!isBlocked(startX, startY, target[0], target[1], m, n, 1)) {
-                    min = Math.min(min, getDistance(m, n, startX, startY, target[0], target[1], 1));
+                else if(dir == 1) {
+                    if(startY > target[1] && startX == target[0]) 
+                        continue;
                 }
-            }
-
-            // 왼쪽 벽 반사
-            if (!(startX > target[0] && startY == target[1])) {
-                if (!isBlocked(startX, startY, target[0], target[1], m, n, 2)) {
-                    min = Math.min(min, getDistance(m, n, startX, startY, target[0], target[1], 2));
+                else if(dir == 2) {
+                    if(startY == target[1] && startX > target[0]) 
+                        continue;
                 }
-            }
-
-            // 오른쪽 벽 반사
-            if (!(startX < target[0] && startY == target[1])) {
-                if (!isBlocked(startX, startY, target[0], target[1], m, n, 3)) {
-                    min = Math.min(min, getDistance(m, n, startX, startY, target[0], target[1], 3));
+                else {
+                    if(startY == target[1] && startX < target[0]) 
+                        continue;
                 }
+                
+                min = Math.min(min, getDistance(dir, m, n, startX, startY, target[0], target[1]));
             }
             
             answer[idx++] = min;
@@ -41,58 +34,27 @@ class Solution {
         return answer;
     }
     
-    // 검은 공이 경로를 방해하는지 확인하는 함수
-    public boolean isBlocked(int sX, int sY, int tX, int tY, int maxX, int maxY, int dir) {
-        int bx = 0, by = 0; // 검은 공의 좌표는 balls 배열에서 가져온다고 가정
-        
-        if (dir == 0) {
-            // 위쪽 벽 반사
-            int reflectedTX = tX;
-            int reflectedTY = 2 * maxY - tY;
-            return (bx == reflectedTX && by > sY && by < reflectedTY); // 검은 공이 경로에 있다면
-        } else if (dir == 1) {
-            // 아래쪽 벽 반사
-            int reflectedTX = tX;
-            int reflectedTY = -tY;
-            return (bx == reflectedTX && by < sY && by > reflectedTY); // 검은 공이 경로에 있다면
-        } else if (dir == 2) {
-            // 왼쪽 벽 반사
-            int reflectedTX = -tX;
-            int reflectedTY = tY;
-            return (by == reflectedTY && bx > sX && bx < reflectedTX); // 검은 공이 경로에 있다면
-        } else {
-            // 오른쪽 벽 반사
-            int reflectedTX = 2 * maxX - tX;
-            int reflectedTY = tY;
-            return (by == reflectedTY && bx < sX && bx > reflectedTX); // 검은 공이 경로에 있다면
-        }
-    }
     
-    public int getDistance(int maxX, int maxY, int sX, int sY, int tX, int tY, int dir) {
+    public int getDistance(int dir, int maxX, int maxY, int sX, int sY, int tX, int tY) {
         int distance = 0;
         
-        if (dir == 0) {
-            // 위쪽 벽 반사
-            int reflectedTX = tX;
-            int reflectedTY = 2 * maxY - tY;
-            distance = (int) (Math.pow(sX - reflectedTX, 2) + Math.pow(sY - reflectedTY, 2));
-        } else if (dir == 1) {
-            // 아래쪽 벽 반사
-            int reflectedTX = tX;
-            int reflectedTY = -tY;
-            distance = (int) (Math.pow(sX - reflectedTX, 2) + Math.pow(sY - reflectedTY, 2));
-        } else if (dir == 2) {
-            // 왼쪽 벽 반사
-            int reflectedTX = -tX;
-            int reflectedTY = tY;
-            distance = (int) (Math.pow(sX - reflectedTX, 2) + Math.pow(sY - reflectedTY, 2));
-        } else {
-            // 오른쪽 벽 반사
-            int reflectedTX = 2 * maxX - tX;
-            int reflectedTY = tY;
-            distance = (int) (Math.pow(sX - reflectedTX, 2) + Math.pow(sY - reflectedTY, 2));
+        if(dir == 0) {
+            // 위
+            distance += Math.pow((maxY * 2 - (sY + tY)), 2) + Math.pow(sX - tX, 2);
         }
-        
+        else if (dir == 1) {
+            // 아래
+            distance += Math.pow((sY + tY), 2) + Math.pow(sX - tX, 2);
+        }
+        else if (dir == 2) {
+            // 왼쪽
+            distance += Math.pow(sY - tY, 2) + Math.pow((sX + tX), 2);
+        }
+        else {
+            // 오른쪽 찍고 오는 경우
+            distance += Math.pow(sY - tY, 2) + Math.pow(maxX * 2 - (sX + tX), 2);
+        }
+   
         return distance;
     }
 }
