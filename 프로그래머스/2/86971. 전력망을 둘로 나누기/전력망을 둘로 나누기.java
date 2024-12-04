5b1099cd23edb000ddfca7497a7d9db8e5fac95c
Jongyoul <dlawhdfbf12@naver.com>
2024-12-04 19:11:00 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.25 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -3,45 +3,51 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[][] wires) {
         int answer = Integer.MAX_VALUE;
-        List<List<Integer>> tree;
-       
-        // 정보 입력
-        for(int i = 0; i < wires.length; i++) {
-            // tree 생성
-            tree = new ArrayList<>();        
-            for(int j = 0; j <= n; j++)
-                tree.add(new ArrayList<>());
-            
-            for(int j = 0; j < wires.length; j++) {
-                // 하나씩 제외하면서 시행
-                if(i == j) continue;
-                tree.get(wires[j][0]).add(wires[j][1]);
-                tree.get(wires[j][1]).add(wires[j][0]);
-            }
-            
-            // 1부터 탐색하고 남은 정점들은 못간거임
-            boolean [] visit = new boolean [n + 1];
-            //dfs
-            visit[1] = true;
-            dfs(1, tree, visit);
-            int checked = 0;
-            for(int j = 1; j <= n; j++)
-                if(visit[j]) checked++;
-            
-            answer = Math.min(answer, Math.abs(n - 2 * checked));
+        List<List<Integer>> tree = new ArrayList<>();
+
+        // tree 생성
+        for (int i = 0; i <= n; i++) {
+            tree.add(new ArrayList<>());
         }
-        
+
+        for (int[] wire : wires) {
+            tree.get(wire[0]).add(wire[1]);
+            tree.get(wire[1]).add(wire[0]);
+        }
+
+        // 각 전선을 끊고 비교
+        for (int[] wire : wires) {
+            int a = wire[0], b = wire[1];
+
+            // 연결 끊기
+            tree.get(a).remove((Integer) b);
+            tree.get(b).remove((Integer) a);
+
+            // 트리의 크기 계산
+            boolean[] visit = new boolean[n + 1];
+            int subtreeSize = dfs(1, tree, visit);
+
+            // 차이 계산
+            answer = Math.min(answer, Math.abs(n - 2 * subtreeSize));
+
+            // 연결 복구
+            tree.get(a).add(b);
+            tree.get(b).add(a);
+        }
+
         return answer;
     }
-    
-    public void dfs (int start, List<List<Integer>> tree, boolean [] visit) {
-        for(int i = 0; i < tree.get(start).size(); i++) {
-            int currNode = tree.get(start).get(i);
-            
-            if(!visit[currNode]) {
-                visit[currNode] = true;
-                dfs(currNode, tree, visit);
+
+    public int dfs(int node, List<List<Integer>> tree, boolean[] visit) {
+        visit[node] = true;
+        int size = 1;
+
+        for (int neighbor : tree.get(node)) {
+            if (!visit[neighbor]) {
+                size += dfs(neighbor, tree, visit);
             }
         }
+
+        return size;
     }
 }
