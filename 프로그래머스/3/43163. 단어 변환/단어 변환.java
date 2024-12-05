89dc986a919a86d5192dfa6222bd3dedf360d3ed
Jongyoul <dlawhdfbf12@naver.com>
2024-12-05 14:29:59 +0900

    [level 3] Title: 단어 변환, Time: 0.61 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,61 +1,57 @@
 import java.util.*;
 
 class Solution {
-    public int solution(String begin, String target, String[] words) {
-        // target이 words에 없으면 변환이 불가능하므로 0을 바로 반환
-        if (!Arrays.asList(words).contains(target)) {
-            return 0;
+    public class Node {
+        String value;
+        int cnt;
+        
+        Node (String value, int cnt) {
+            this.value = value;
+            this.cnt = cnt;
         }
+    }
+    
+    public int solution(String begin, String target, String[] words) {
+        Queue<Node> q = new LinkedList<>();
+        boolean [] visit = new boolean [words.length];
+        
+        for(int i = 0; i < words.length; i++) {
+            if(isValid(begin, words[i])) {
+                
+                if(words[i].equals(target))
+                    return 1;
+                
+                q.add(new Node(words[i], 1));
+                visit[i] = true;
+            }
+        }
+        
+        while(!q.isEmpty()) {
+            Node curr = q.poll();
+            String currAlp = curr.value;
+            int nextCnt = curr.cnt + 1;
+            
+            for(int i = 0; i < words.length; i++) {
+                if(!visit[i] && isValid(currAlp, words[i])) {
+                    if(words[i].equals(target))
+                        return nextCnt;
 
-        // BFS를 위한 큐 선언
-        Queue<String> queue = new LinkedList<>();
-        // 큐에는 단어와 그 단어로 가기 위한 단계수를 함께 저장
-        queue.offer(begin);
-        int steps = 0;
-
-        // 방문 체크 배열
-        boolean[] visited = new boolean[words.length];
-
-        // BFS 시작
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            steps++;
-
-            // 큐에 있는 모든 단어를 한 단계씩 처리
-            for (int i = 0; i < size; i++) {
-                String current = queue.poll();
-
-                // 모든 words 배열을 돌면서 변환 가능한 단어를 큐에 추가
-                for (int j = 0; j < words.length; j++) {
-                    // 이미 방문한 단어는 건너뛰기
-                    if (visited[j]) continue;
-
-                    // 한 글자만 다르면 변환 가능
-                    if (isOneCharDiff(current, words[j])) {
-                        if (words[j].equals(target)) {
-                            return steps;  // target에 도달하면 그 단계 수를 반환
-                        }
-                        queue.offer(words[j]);  // 변환 가능한 단어 큐에 추가
-                        visited[j] = true;  // 방문 처리
-                    }
+                    q.add(new Node(words[i], nextCnt));
+                    visit[i] = true;
                 }
             }
         }
         
-        return 0;  // 큐를 다 돌아도 target을 찾지 못하면 변환 불가능
+        return 0;
     }
-
-    // 두 단어가 한 글자만 다른지 확인하는 함수
-    private boolean isOneCharDiff(String word1, String word2) {
-        int diffCount = 0;
-        for (int i = 0; i < word1.length(); i++) {
-            if (word1.charAt(i) != word2.charAt(i)) {
-                diffCount++;
-                if (diffCount > 1) {
-                    return false;
-                }
-            }
+    
+    public boolean isValid(String obj, String target) {
+        int cnt = 0;
+        for(int i = 0; i < obj.length(); i++) {
+            if(obj.charAt(i) != target.charAt(i))
+                cnt++;
         }
-        return diffCount == 1;
+        
+        return cnt == 1;
     }
 }
