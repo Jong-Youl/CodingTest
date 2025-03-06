47bd980f629529c56ee7306dd79943ef1213e01e
Jongyoul <dlawhdfbf12@naver.com>
2025-03-06 11:04:41 +0900

    [level 1] Title: 소수 찾기, Time: 120.11 ms, Memory: 83.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int solution(int n) {
+        if(n < 2)
+            return 0;
+        if(n == 2)
+            return 1;
+        
+        int answer = 1;
+        for(int i = 3; i <= n; i += 2) {
+            if(isPrime(i))
+                answer++;
+        }
+        
+        return answer;
+    }
+    
+    public boolean isPrime(int n) {
+        if(n < 2)
+            return false;
+        if(n == 2)
+            return true;
+        if(n == 3)
+            return true;
+        
+        for(int i = 3; i <= Math.sqrt(n); i+=2) {
+            if(n % i == 0)
+                return false;
+        }
+        
+        return true;
+    }
+}
