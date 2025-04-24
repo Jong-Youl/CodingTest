9c08b2bedcc141c49ff3fc4fe271a6ede9cd71b2
Jongyoul <dlawhdfbf12@naver.com>
2025-04-24 09:28:46 +0900

    [level 2] Title: [3차] n진수 게임, Time: 16.93 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -8,14 +8,12 @@ class Solution {
 
         while (seq.size() < t * m) {
             String curr = Integer.toString(num++, n).toUpperCase();
-            for (char ch : curr.toCharArray()) {
+            for (char ch : curr.toCharArray()) 
                 seq.add(ch);
-            }
         }
 
-        for (int i = 0; i < t; i++) {
+        for (int i = 0; i < t; i++)
             answer.append(seq.get(i * m + (p - 1)));
-        }
 
         return answer.toString();
     }
