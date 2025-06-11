d42f48f0edf290fcdbac74bfaada0af29bd870a2
Jongyoul <dlawhdfbf12@naver.com>
2025-06-11 23:48:33 +0900

    [level 2] Title: 타겟 넘버, Time: 4.11 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,20 +1,21 @@
 class Solution {
     public int answer = 0;
     public int solution(int[] numbers, int target) {
-        // 덧셈 or 뺄셈      
-        dfs(0, 0, numbers, target);
+        // 더하거나 빼서 타겟 넘버를 만들것
+        // 덧셈 뺄셈은 숫자를 바꿔도 같음
+        dfs(numbers, 0, 0, target);
         return answer;
     }
     
-    public void dfs (int depth, int sum, int [] nums, int target) {
-        if (depth == nums.length) {
-            if(sum == target) {
-                answer++;             
-            }
+    public void dfs(int [] numbers, int depth, int sum, int target) {
+        if(depth == numbers.length) {
+            if(sum == target)
+                answer++;
+            
             return;
         }
         
-        dfs(depth + 1, sum + nums[depth], nums, target);
-        dfs(depth + 1, sum - nums[depth], nums, target);
+        dfs(numbers, depth + 1, sum + numbers[depth], target);
+        dfs(numbers, depth + 1, sum - numbers[depth], target);
     }
 }
