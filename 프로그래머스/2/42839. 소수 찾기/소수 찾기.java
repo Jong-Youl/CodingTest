ba2fbae30cfd19ed7acd4ee45899948b16d4a930
Jongyoul <dlawhdfbf12@naver.com>
2025-06-14 09:22:13 +0900

    [level 2] Title: 소수 찾기, Time: 1429.29 ms, Memory: 392 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,44 +1,70 @@
 import java.util.*;
 
 class Solution {
+    
+    public Set<Integer> set;
+
     public int solution(String numbers) {
         int answer = 0;
-        Set<Integer> set = new HashSet<>();
-        boolean[] visit = new boolean[numbers.length()];
+        set = new HashSet<>();
+        StringBuilder sb = new StringBuilder();
+        boolean [] visit = new boolean [numbers.length()];
         
-        comb(0, "", numbers, visit, set);
+        comb(0, new StringBuilder(), numbers, visit);
         
-        for (int num : set) {
-            if (isPrime(num)) {
+        for(int num : set) {
+            if(isPrime(num)) 
                 answer++;
-            }
+
         }
-        
+
         return answer;
     }
-
-    private void comb(int depth, String curr, String numbers, boolean[] visit, Set<Integer> set) {
-        if (!curr.isEmpty()) {
-            set.add(Integer.parseInt(curr));
-        }
-        if (depth == numbers.length()) {
+    
+    public void comb (int depth, StringBuilder sb, String numbers, boolean [] visit) {
+        if(depth == numbers.length()) {
+            if(sb.length() == 0)
+                return;
+            String tmp = sb.toString();
+            set.add(Integer.parseInt(tmp));
             return;
         }
         
-        for (int i = 0; i < numbers.length(); i++) {
-            if (!visit[i]) {
-                visit[i] = true;
-                comb(depth + 1, curr + numbers.charAt(i), numbers, visit, set);
-                visit[i] = false;
-            }
+        for(int i = 0; i < numbers.length(); i++) {
+            if(visit[i])
+                continue;
+            
+            int curr = numbers.charAt(i) - '0';
+            // 넣지 않음
+            comb(depth + 1, sb, numbers, visit);
+            // 넣음
+            StringBuilder currSb = new StringBuilder(sb.toString());
+            if(curr == 0 && currSb.length() == 0)
+                continue;
+            
+            currSb.append(curr);
+            visit[i] = true;
+            comb(depth + 1, currSb, numbers, visit);
+            visit[i] = false;
         }
     }
 
-    private boolean isPrime(int num) {
-        if (num <= 1) return false;
-        for (int i = 2; i <= Math.sqrt(num); i++) {
-            if (num % i == 0) return false;
+        
+    public boolean isPrime (int num) {
+        if(num <= 1)
+            return false;
+        if(num <= 3)
+            return true;
+        
+        if(num % 2 == 0)
+            return false;
+        
+        for(int i = 3; i <= Math.sqrt(num); i += 2) {
+            if(num % i == 0)
+                return false;
         }
+        
         return true;
     }
+    
 }
